d320afd0d5093442eb08c8b350c050bda994ed6b
Mastersam <abadasamuelosp@gmail.com>
2020-05-21 12:26:03 -0700

    refactor to PEP8 style

--- signer.py
+++ signer.py
@@ -23,23 +23,39 @@ def createdAt(filePath):
 
 # Generates signature content
 def generateSignature(symbol, name, date):
-    signature_one = "{open}\n**\n * author :   \t{name}\n * created : \t{date}\n**\n{close}\n"
+    signature_one = "{open}\n**\n * author :   " \
+                    "\t{name}\n * created : " \
+                    "\t{date}\n**\n{close}\n"
 
-    signature_mul = "{open}\n{open}**\n{open} * author :   \t{name}\n{open} * created : \t{date}\n{open}**\n{close}\n"
+    signature_mul = "{open}\n{open}**\n{open} * author :   " \
+                    "\t{name}\n{open} * created : " \
+                    "\t{date}\n{open}**\n{close}\n"
 
     if symbol[2]:
 
-        return signature_mul.format(open=symbol[0], close=symbol[1], name=name, date=date)
+        return signature_mul.format(open=symbol[0],
+                                    close=symbol[1],
+                                    name=name,
+                                    date=date,
+                                    )
 
     else:
 
-        return signature_one.format(open=symbol[0], close=symbol[1], name=name, date=date)
+        return signature_one.format(open=symbol[0],
+                                    close=symbol[1],
+                                    name=name,
+                                    date=date,)
 
 
 # Checks if file has already been signed before
 def fileAlreadySigned(contents, signature):
-    return signature[0:40] in contents[
-                              0:50]  # checking 0-50th index of the string instead of the whole string, to increase search time
+    """
+    checking 0-50th index of the string instead of the whole string,
+    to increase search time
+    :param contents:
+    :param signature:
+    """
+    return signature[0:40] in contents[0:50]
 
 
 # Signs a file
@@ -72,7 +88,8 @@ def getSymbol(extension):
 
 def Sign(dir_path, author, *args):
     # check that argument specified is a valid file or directory
-    assert (path.isdir(dir_path) or path.isfile(dir_path)), "You have to specify a directory by setting --dir_path"
+    assert (path.isdir(dir_path) or path.isfile(dir_path)), \
+        "You have to specify a directory by setting --dir_path"
     # check that there are files presents in the specified directory
     if path.isdir(dir_path):
         assert (len(listdir(dir_path)) > 0), "No files to sign here"
--- test/test.py
+++ test/test.py
@@ -4,15 +4,19 @@ import signer
 import symbols
 from random import randint
 
+
 class AppTests(unittest.TestCase):
     """
-    Ensure to add the project to PYTHON_PATH in order to make the package visible to this test file
+    Ensure to add the project to PYTHON_PATH
+    in order to make the package visible to this test file
     Also, make sure you run this test from the test directory
     """
+
     def test_sign(self):
 
         def randomName():
-            return "".join("abcdefghijklmnopqrstuvwxyz"[randint(0,100)%26] for i in range(5))
+            return "".join("abcdefghijklmnopqrstuvwxyz"
+                           [randint(0, 100) % 26] for i in range(5))
 
         # current working directory must be the /test directory
         pathToTestFile = os.path.join(os.getcwd() + "/sample/")
@@ -20,19 +24,20 @@ class AppTests(unittest.TestCase):
 
         for file in samplefiles:
             # Lets deal with files
-            if len(file.split("."))>0 and file.split(".")[-1] in symbols.symbols:
+            if len(file.split(".")) > 0 and \
+                    file.split(".")[-1] in symbols.symbols:
                 with open(pathToTestFile + file, "r") as f:
                     # keep the old file contents before signing
                     oldContent = f.read()
                 author = randomName()
                 # sign test files
-                signer.Sign(pathToTestFile+file, author)
-                with open(pathToTestFile+file, "r") as f:
+                signer.Sign(pathToTestFile + file, author)
+                with open(pathToTestFile + file, "r") as f:
                     self.assertTrue(author in f.read())
                 # write back the old file contents after signing
                 with open(pathToTestFile + file, "w") as f:
                     f.write(oldContent)
 
+
 if __name__ == "__main__":
     unittest.main()
-
